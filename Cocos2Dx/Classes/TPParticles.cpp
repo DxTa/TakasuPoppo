d8af98876fb2bc836db2946941d75473cab55273
thevinh <nguyen.the.vinh@framgia.com>
2013-08-17 16:36:37 +0700

    push

--- Cocos2Dx/Classes/TPParticles.cpp
+++ Cocos2Dx/Classes/TPParticles.cpp
@@ -329,25 +329,25 @@ void TakasuPoppo::generationEffect(CCPoint startPoint, CCPoint destination) {
 }
 
 void TakasuPoppo::exPopBlast(CCPoint position) {
-    CCSprite *exBlast = CCSprite::create();
-    exBlast->setPosition(position);
-    
-    CCSpriteFrameCache* cache = CCSpriteFrameCache::sharedSpriteFrameCache();
-    cache->addSpriteFramesWithFile("exPop.plist");
-    CCArray* animFrames = new CCArray;
-    animFrames->autorelease();
-    char str[100] = {0};
-    for(int i = 0; i < 9; i++) {
-        sprintf(str, "exPop%d.png", i);
-        CCSpriteFrame* frame = cache->spriteFrameByName( str );
-        frame->setOriginalSizeInPixels(CCSizeMake(300, 300));
-        animFrames->addObject(frame);
-    }
-    CCAnimation* animation = CCAnimation::createWithSpriteFrames(animFrames, 0.1);
-    CCAnimate *animate = CCAnimate::create(animation);
-    
-    this->addChild(exBlast, 16, 1111);
-    exBlast->runAction(animate);
+//    CCSprite *exBlast = CCSprite::create();
+//    exBlast->setPosition(position);
+//    
+//    CCSpriteFrameCache* cache = CCSpriteFrameCache::sharedSpriteFrameCache();
+//    cache->addSpriteFramesWithFile("exPop.plist");
+//    CCArray* animFrames = new CCArray;
+//    animFrames->autorelease();
+//    char str[100] = {0};
+//    for(int i = 0; i < 9; i++) {
+//        sprintf(str, "exPop%d.png", i);
+//        CCSpriteFrame* frame = cache->spriteFrameByName( str );
+////        frame->setOriginalSizeInPixels(CCSizeMake(1260, 1260));
+//        animFrames->addObject(frame);
+//    }
+//    CCAnimation* animation = CCAnimation::createWithSpriteFrames(animFrames, 0.1);
+//    CCAnimate *animate = CCAnimate::create(animation);
+//    
+//    this->addChild(exBlast, 16, 1111);
+//    exBlast->runAction(animate);
 }
 
 
